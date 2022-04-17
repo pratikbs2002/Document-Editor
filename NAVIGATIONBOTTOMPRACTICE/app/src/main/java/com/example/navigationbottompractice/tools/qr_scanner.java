1e7355ff29afbae9a4d2b63012992b9eedf3a0b1
pratikbs2002 <20ce141@charusat.edu.in>
2022-04-17 21:46:14 +0530

    qr scanner

--- NAVIGATIONBOTTOMPRACTICE/app/src/main/java/com/example/navigationbottompractice/tools/qr_scanner.java
+++ NAVIGATIONBOTTOMPRACTICE/app/src/main/java/com/example/navigationbottompractice/tools/qr_scanner.java
@@ -2,7 +2,11 @@ package com.example.navigationbottompractice.tools;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.navigationbottompractice.R;
@@ -11,10 +15,21 @@ import java.util.Objects;
 
 public class qr_scanner extends AppCompatActivity {
 Toolbar toolbar;
+    Button btnScanBarcode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_scanner);
+
+        btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        btnScanBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(qr_scanner.this, com.example.navigationbottompractice.qr_scanning.class));
+            }
+        });
+
 
         toolbar = findViewById(R.id.main_tool_bar1);
         setTitle("QR SCANNER");
@@ -30,3 +45,5 @@ Toolbar toolbar;
 
     }
 }
+
+
